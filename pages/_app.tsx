3c0990a5f519ad99790b3fa3e019d213e8ab65cb
Do Hyeong Kim <dhk21512@gmail.com>
2023-04-14 16:47:27 +0900

    paths setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div>App1</div>
+      {/* Component는 index.tsx 여기에 Recoil같은거 감싸기 I18Provider, styledComponent */}
+      <Component {...pageProps} />
+      <div>App2</div>
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <div>Doc1</div>
+        {/* MAIN 은 _app.tsx */}
         <Main />
+        <div>Doc2</div>
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
